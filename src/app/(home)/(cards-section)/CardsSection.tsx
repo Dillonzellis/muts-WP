f3c3c63903c65696e5cc2bdffa045d52b093bb56
Dillon Ellis <dillonzellis@gmail.com>
2023-05-30 10:37:06 -0400

    cards section header text

--- src/app/(home)/(cards-section)/CardsSection.tsx
+++ src/app/(home)/(cards-section)/CardsSection.tsx
@@ -7,11 +7,16 @@ const CardSection = () => {
   return (
     <div className="pb-20">
       <div className="relative">
+        <h2 className="absolute left-1/2 top-20 max-w-3xl -translate-x-1/2 text-center text-3xl font-bold text-white">
+          We&apos;re a leading provider of short-term and long-term needs for
+          all of our members
+        </h2>
         <Image
+          className="lg:px-22 mx-auto px-4 sm:px-6"
           src="https://dev2.growthbydesign.org/wp-content/uploads/home-cta.jpg"
           alt="1"
-          width={1920}
-          height={1200}
+          width={1700}
+          height={870}
         />
         <CardDeck />
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import CardSection from './(home)/(cards-section)/CardsSection';
-import Services from './(home)/(services)/Services';
-import CTA from './(home)/CTA';
-import Hero from './(home)/Hero';
-import Container from './components/(layout)/Container';
+import CardSection from "./(home)/(cards-section)/CardsSection";
+import Services from "./(home)/(services)/Services";
+import CTA from "./(home)/CTA";
+import Hero from "./(home)/Hero";
+import Container from "./components/(layout)/Container";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <CardSection />
       <Container>
-        <CardSection />
         <CTA />
         <Services />
       </Container>
